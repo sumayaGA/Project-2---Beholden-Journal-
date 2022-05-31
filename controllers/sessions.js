095304719f41ffaae5670d4a5004a25faa6c741f
Sumaya Ali <sumayaali@Sumayas-iMac.local>
2022-05-31 16:05:41 -0500

    Adds user authentication

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,6 +4,33 @@ const bcrypt = require("bcrypt");
 const sessionsRouter = express.Router();
 const User = require("../models/user");
 
+//Create (login route)
+sessionsRouter.post("/", (req,res)=>{
+    User.findOne({
+        email:req.body.email
+    }, (error, foundUser)=>{
+        if(!foundUser){
+            res.send("Whoops-a-daisy! No user with that email address has been registered.");
+        } else {
+            const passwordMatches = bcrypt.compareSync(req.body.password, foundUser.password);
+
+                if (passwordMatches) {
+                    req.session.currentUser = foundUser;
+                    res.redirect("/");
+                }else{
+                    res.send("Whoops-a-daisy! Invalid credentials.");
+                }
+        }
+
+    });
+});
+
+//Delete (logout route)
+sessionsRouter.delete("/", (req,res)=> {
+    req.session.destroy((error)=>{
+        res.redirect("/");
+    });
+});
 
 //Export Sessions Router
 module.exports = sessionsRouter;
