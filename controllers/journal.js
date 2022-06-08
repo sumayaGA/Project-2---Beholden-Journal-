756e997635ac3dbe641d89320a84cfa9abdf2e17
Sumaya Ali <sumayaali@Sumayas-iMac-2.local>
2022-06-08 18:02:19 -0500

    Index and Show routes working

--- controllers/journal.js
+++ controllers/journal.js
@@ -6,6 +6,15 @@ const User = require("../models/user");
 const Journal = require("../models/entry");
 
 
+//Index
+journalRouter.get("/", (req, res) => {
+    Journal.find ({}, (error, allJournal)=> {
+        res.render("all-entries/index.ejs", { 
+            journals: allJournal
+        });
+    });
+});
+
 //New
 journalRouter.get("/new", (req,res)=>{
     res.render("new.ejs", {
@@ -13,11 +22,35 @@ journalRouter.get("/new", (req,res)=>{
     });
 });
 
+//DELETE
+journalRouter.delete("/:id", (req,res)=>{
+    Journal.findByIdAndRemove(req.params.id, (err,data)=> {
+        res.redirect("/journal");
+    });
+});
 
 //Create
 journalRouter.post("/", (req,res)=>{
     Journal.create(req.body, (error, createdJournal)=>{
         res.redirect("/");
+    });
+});
+
+//EDIT
+journalRouter.get("/:id/edit", (req,res)=>{
+    Journal.findById(req.params.id, (err, foundJournal)=> {
+        res.render("edit.ejs", {
+            journal:foundJournal
+        });
+    });
+});
+
+//SHOW
+journalRouter.get("/:id", (req,res)=>{
+    Journal.findById(req.params.id, (err, foundJournal) => {
+        res.render("all-entries/show.ejs", {
+            journal:foundJournal
+        });
     });
 });
 
--- models/entry.js
+++ models/entry.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const entrySchema = new mongoose.Schema ({
     title: {type:"string", require:true},
     content:{type: "string", require:true},
-    date: {type: "date", require:true}
+    date: {type:"date", require:true}
 });
 
 const Journal = mongoose.model("Journal", entrySchema);
